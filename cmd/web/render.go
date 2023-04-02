d3241c2187d96a547ff4d1bb6a0e4550440a70f5
kody.low <kodylow7@gmail.com>
2023-04-02 10:29:18 -0700

    add data package for models, plan and user

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -12,53 +12,57 @@ var pathToTemplates = "./cmd/web/templates"
 type TemplateData struct {
 	StringMap     map[string]string
 	IntMap        map[string]int
-	FloatMap      map[string]float32
+	FloatMap      map[string]float64
 	Data          map[string]any
 	Flash         string
 	Warning       string
 	Error         string
 	Authenticated bool
 	Now           time.Time
-	// User	   *data.User
+	// User *data.User
 }
 
 func (app *Config) render(w http.ResponseWriter, r *http.Request, t string, td *TemplateData) {
 	partials := []string{
 		fmt.Sprintf("%s/base.layout.gohtml", pathToTemplates),
 		fmt.Sprintf("%s/header.partial.gohtml", pathToTemplates),
-		fmt.Sprintf("%s/footer.partial.gohtml", pathToTemplates),
 		fmt.Sprintf("%s/navbar.partial.gohtml", pathToTemplates),
+		fmt.Sprintf("%s/footer.partial.gohtml", pathToTemplates),
 		fmt.Sprintf("%s/alerts.partial.gohtml", pathToTemplates),
 	}
 
-	var ts []string
-	ts = append(ts, fmt.Sprintf("%s/%s", pathToTemplates, t))
-	ts = append(ts, partials...)
+	var templateSlice []string
+	templateSlice = append(templateSlice, fmt.Sprintf("%s/%s", pathToTemplates, t))
+
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
 
 	if td == nil {
 		td = &TemplateData{}
 	}
 
-	tmpl, err := template.ParseFiles(ts...)
+	tmpl, err := template.ParseFiles(templateSlice...)
 	if err != nil {
 		app.ErrorLog.Println(err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if err := tmpl.Execute(w, app.AddDefaultData(td, r)); err != nil {
 		app.ErrorLog.Println(err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
 
 func (app *Config) AddDefaultData(td *TemplateData, r *http.Request) *TemplateData {
-	td.Flash == app.Session.PopString(r.Context(), "flash")
-	td.Warning == app.Session.PopString(r.Context(), "warning")
-	td.Error == app.Session.PopString(r.Context(), "error")
+	td.Flash = app.Session.PopString(r.Context(), "flash")
+	td.Warning = app.Session.PopString(r.Context(), "warning")
+	td.Error = app.Session.PopString(r.Context(), "error")
 	if app.IsAuthenticated(r) {
 		td.Authenticated = true
-		// TODO: get user data
+		// TODO - get more user information
 	}
 	td.Now = time.Now()
 
